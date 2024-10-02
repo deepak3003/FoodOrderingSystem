702d81f15898b2f1ce1a140a64bedf857bc3ba4c
deepak3003 <deepaksingh76152@gmail.com>
2024-10-02 11:56:08 +0530

    Improving the code by breaking it into smaller modules

--- MealOrderingSystem.java
+++ MealOrderingSystem.java
@@ -4,9 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class MealOrderingSystem {
-
-    public static void menu() {
+class Menu {
+    public void displayMenu() {
         System.out.println("1. Burger - ₹50.00");
         System.out.println("2. Pizza - ₹80.00");
         System.out.println("3. Sandwich - ₹40.00");
@@ -14,56 +13,72 @@ public class MealOrderingSystem {
         System.out.println("5. Exit");
 
     }
+}
 
-    public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            int choice;
-            String order = "";
-            List<String> orders = new ArrayList<>();
+class Order {
+    private final List<String> orders = new ArrayList<>();
+    private double total = 0;
 
-            double total = 0;
+    public void addItem(int choice) {
+        switch (choice) {
+            case 1:
+                total += 50;
+                System.out.println("You added a Burger");
+                orders.add("Burger");
+                break;
+            case 2:
+                total += 80;
+                System.out.println("You added a Pizza");
+                orders.add("Pizza");
+                break;
+            case 3:
+                total += 40;
+                System.out.println("You added a Sandwich");
+                orders.add("Sandwich");
+                break;
+            case 4:
+                total += 20;
+                System.out.println("You added a Coke");
+                orders.add("Coke");
+                break;
 
-            while (!order.equalsIgnoreCase("n")) {
-                menu();
-                System.out.print("Please select an item (1-5): ");
-                choice = sc.nextInt();
 
-                switch (choice) {
-                    case 1:
-                        total += 50;
-                        System.out.println("You added a Burger");
-                        orders.add("Burger");
-                        break;
-                    case 2:
-                        total += 80;
-                        System.out.println("You added a Pizza");
-                        orders.add("Pizza");
-                        break;
-                    case 3:
-                        total += 40;
-                        System.out.println("You added a Sandwich");
-                        orders.add("Sandwich");
-                        break;
-                    case 4:
-                        total += 20;
-                        System.out.println("You added a Coke");
-                        orders.add("Coke");
-                        break;
-                    case 5:
-                        order = "n";
-                        continue;
-
-                    default:
-                        System.out.println("Invalid selection.");
-                }
-                sc.nextLine();
-                System.out.println("Would you like to order anything else? (y/n)");
-                order = sc.nextLine();
-            }
-            System.out.print("You ordered a ");
-            System.out.println(orders);
-            System.out.println("Your total is: ₹" + total + "\nThank you for your order!");
+            default:
+                System.out.println("Invalid selection.");
         }
     }
+
+    public void displayOrder() {
+        System.out.print("You ordered a ");
+        System.out.println(orders);
+        System.out.println("Your total is: ₹" + total + "\nThank you for your order!");
+
+    }
+}
+
+public class MealOrderingSystem {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        Order order = new Order();
+        try (Scanner sc = new Scanner(System.in)) {
+
+            String orderMore = "";
+            while (!orderMore.equalsIgnoreCase("n")) {
+                menu.displayMenu();
+                System.out.print("Please select an item (1-5): ");
+                int choice = sc.nextInt();
+                if (choice == 5) {
+                    break;
+                }
+                order.addItem(choice);
+                sc.nextLine();
+                System.out.println("Would you like to order anything else? (y/n)");
+                orderMore = sc.nextLine();
+            }
+            order.displayOrder();
+
+        }
+    }
+
 
 }
